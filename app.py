b273214dc2fbe94cf25c0528f5e427af12106cfc
Ch israr <95218955+Ch-israr@users.noreply.github.com>
2025-09-25 22:28:06 +0400

    Update app.py

--- app.py
+++ app.py
@@ -343,7 +343,4 @@ def internal_err(e):
     return render_template("error.html", message="Internal server error"), 500
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
-
-
+    app.run()
